7cc7e5ff7a82adcc02ec3f718ca3e9986e94371a
マスター <master@jibunnoMacBook-Pro.local>
2025-07-26 16:36:42 +0900

    ”fix/マイグレーションファイルを追加”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_21_143414) do
+ActiveRecord::Schema.define(version: 2025_07_26_070016) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,7 +59,8 @@ ActiveRecord::Schema.define(version: 2025_07_21_143414) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +81,5 @@ ActiveRecord::Schema.define(version: 2025_07_21_143414) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "reservations", "users"
+  add_foreign_key "rooms", "users"
 end
